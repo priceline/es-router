ac85407bc14c21cb2a64c365c831bfc8fffd823d
Evan Pipta <evan.pipta@priceline.com>
2017-02-06 11:15:41 -0500

    adding optional param

--- es-router.js
+++ es-router.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -20,11 +20,12 @@ var EsRouter = function () {
   function EsRouter(_ref) {
     var _this = this;
 
-    var useHash = _ref.useHash;
-    var routes = _ref.routes;
-    var notStrictRouting = _ref.notStrictRouting;
-    var home = _ref.home;
-    var base = _ref.base;
+    var useHash = _ref.useHash,
+        routes = _ref.routes,
+        notStrictRouting = _ref.notStrictRouting,
+        home = _ref.home,
+        base = _ref.base,
+        routeOnLoad = _ref.routeOnLoad;
 
     _classCallCheck(this, EsRouter);
 
@@ -95,7 +96,9 @@ var EsRouter = function () {
     }
 
     //do an initial routing
-    this.path(this.getPathFromUrl());
+    if (routeOnLoad !== false) {
+      this.path(this.getPathFromUrl());
+    }
   }
 
   //get path we're currently on
